185fba7a951e0b1b1587235c181a665c8040d9f6
Rin Iwai <rin@eyrin.jp>
2024-11-05 00:18:17 +0900

    fix

--- include/tutrcos/peripheral/pwm.hpp
+++ include/tutrcos/peripheral/pwm.hpp
@@ -39,13 +39,6 @@ namespace peripheral {
  */
 class PWM {
 public:
-  /**
-   * PWM初期化
-   *
-   * @param htim TIM_HandleTypeDefへのポインタ
-   * @param channel 使用するPWMチャネル
-   * @return true: 成功, false: 失敗
-   */
   PWM(TIM_HandleTypeDef *htim, uint32_t channel)
       : htim_{htim}, channel_{channel} {
     if (HAL_TIM_PWM_Start(htim_, channel_) != HAL_OK) {
@@ -55,14 +48,8 @@ public:
 
   ~PWM() { HAL_TIM_PWM_Stop(htim_, channel_); }
 
-  /**
-   * タイマのカウンタを取得
-   */
   uint32_t get_compare() { return __HAL_TIM_GET_COMPARE(htim_, channel_); }
 
-  /**
-   * タイマのカウンタを設定
-   */
   void set_compare(uint32_t compare) {
     __HAL_TIM_SET_COMPARE(htim_, channel_, compare);
   }
