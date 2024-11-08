ee5a3c91f4ad6f28e74ffb7e93a069ede643dbf7
Rin Iwai <rin@eyrin.jp>
2024-11-09 00:03:14 +0900

    microros動いた!

---
+++ include/tutrcos/module/microros.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <cstddef>
+
+#include <rcl/rcl.h>
+#include <rmw_microros/rmw_microros.h>
+
+#include "tutrcos/peripheral/uart.hpp"
+
+extern "C" {
+
+void *microros_allocate(size_t size, void *state);
+void microros_deallocate(void *pointer, void *state);
+void *microros_reallocate(void *pointer, size_t size, void *state);
+void *microros_zero_allocate(size_t number_of_elements, size_t size_of_element,
+                             void *state);
+
+bool tutrcos_MicroROS_open_cb(uxrCustomTransport *transport);
+bool tutrcos_MicroROS_close_cb(uxrCustomTransport *transport);
+size_t tutrcos_MicroROS_write_cb(uxrCustomTransport *transport,
+                                 const uint8_t *buffer, size_t length,
+                                 uint8_t *error_code);
+size_t tutrcos_MicroROS_read_cb(uxrCustomTransport *transport, uint8_t *buffer,
+                                size_t length, int timeout,
+                                uint8_t *error_code);
+}
+
+namespace tutrcos {
+namespace module {
+
+class MicroROS {
+public:
+  static inline bool init(peripheral::UART &uart) {
+    if (rmw_uros_set_custom_transport(true, &uart, tutrcos_MicroROS_open_cb,
+                                      tutrcos_MicroROS_close_cb,
+                                      tutrcos_MicroROS_write_cb,
+                                      tutrcos_MicroROS_read_cb) != RMW_RET_OK) {
+      return false;
+    }
+
+    rcl_allocator_t allocator = rcutils_get_zero_initialized_allocator();
+    allocator.allocate = microros_allocate;
+    allocator.deallocate = microros_deallocate;
+    allocator.reallocate = microros_reallocate;
+    allocator.zero_allocate = microros_zero_allocate;
+
+    return rcutils_set_default_allocator(&allocator);
+  };
+
+  friend bool ::tutrcos_MicroROS_open_cb(uxrCustomTransport *transport);
+  friend bool ::tutrcos_MicroROS_close_cb(uxrCustomTransport *transport);
+  friend size_t ::tutrcos_MicroROS_write_cb(uxrCustomTransport *transport,
+                                            const uint8_t *buffer,
+                                            size_t length, uint8_t *error_code);
+  friend size_t ::tutrcos_MicroROS_read_cb(uxrCustomTransport *transport,
+                                           uint8_t *buffer, size_t length,
+                                           int timeout, uint8_t *error_code);
+};
+
+} // namespace module
+} // namespace tutrcos
---
+++ src/module/microros.cpp
@@ -0,0 +1,40 @@
+#if __has_include(<rmw_microxrcedds_c/config.h>)
+#include <rmw_microxrcedds_c/config.h>
+#endif
+
+#ifdef RMW_UXRCE_TRANSPORT_CUSTOM
+
+#include <uxr/client/transport.h>
+
+#include "tutrcos/module/microros.hpp"
+
+bool tutrcos_MicroROS_open_cb(uxrCustomTransport *) {
+  // 何もしない
+  return true;
+}
+
+bool tutrcos_MicroROS_close_cb(uxrCustomTransport *) {
+  // 何もしない
+  return true;
+}
+
+size_t tutrcos_MicroROS_write_cb(uxrCustomTransport *transport,
+                                 const uint8_t *buffer, size_t length,
+                                 uint8_t *) {
+  auto uart = reinterpret_cast<tutrcos::peripheral::UART *>(transport->args);
+  if (!uart->transmit(buffer, length)) {
+    return 0;
+  }
+  return length;
+}
+
+size_t tutrcos_MicroROS_read_cb(uxrCustomTransport *transport, uint8_t *buffer,
+                                size_t length, int timeout, uint8_t *) {
+  auto uart = reinterpret_cast<tutrcos::peripheral::UART *>(transport->args);
+  if (!uart->receive(buffer, length, timeout)) {
+    return 0;
+  }
+  return length;
+}
+
+#endif
