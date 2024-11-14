0043113210151659e6b6f5dc109a79073885b2ca
Rin Iwai <rin@eyrin.jp>
2024-11-14 15:50:35 +0900

    fix

--- include/tutrcos/peripheral/i2c_master.hpp
+++ include/tutrcos/peripheral/i2c_master.hpp
@@ -14,47 +14,40 @@ namespace peripheral {
 
 class I2CMaster {
 public:
-  I2CMaster(I2C_HandleTypeDef *hi2c) : hi2c_{hi2c} {
-    get_instances()[hi2c_] = this;
-  }
+  I2CMaster(I2C_HandleTypeDef *hi2c) : hi2c_{hi2c} {}
 
-  bool transmit(uint16_t address, uint8_t *data, size_t size) {
+  bool transmit(uint16_t address, uint8_t *data, size_t size,
+                uint32_t timeout) {
     std::lock_guard lock{mtx_};
-    if (HAL_I2C_Master_Transmit_IT(hi2c_, address << 1, data, size) != HAL_OK) {
-      return false;
+    uint32_t start = core::Kernel::get_ticks();
+    while (HAL_I2C_Master_Transmit_IT(hi2c_, address << 1, data, size) !=
+           HAL_OK) {
+      uint32_t elapsed = core::Kernel::get_ticks() - start;
+      if (elapsed >= timeout) {
+        return false;
+      }
+      core::Thread::delay(1);
     }
-    bool res;
-    if (!res_.pop(res, core::Kernel::MAX_DELAY)) {
-      return false;
-    }
-    return res;
+    return true;
   }
 
   bool receive(uint16_t address, uint8_t *data, size_t size, uint32_t timeout) {
     std::lock_guard lock{mtx_};
-    if (HAL_I2C_Master_Receive_IT(hi2c_, address << 1, data, size) != HAL_OK) {
-      return false;
+    uint32_t start = core::Kernel::get_ticks();
+    while (HAL_I2C_Master_Receive_IT(hi2c_, address << 1, data, size) !=
+           HAL_OK) {
+      uint32_t elapsed = core::Kernel::get_ticks() - start;
+      if (elapsed >= timeout) {
+        return false;
+      }
+      core::Thread::delay(1);
     }
-    bool res;
-    if (!res_.pop(res, timeout)) {
-      return false;
-    }
-    return res;
+    return true;
   }
 
 private:
   I2C_HandleTypeDef *hi2c_;
   core::Mutex mtx_;
-  core::Queue<bool> res_{1};
-
-  static inline std::map<I2C_HandleTypeDef *, I2CMaster *> &get_instances() {
-    static std::map<I2C_HandleTypeDef *, I2CMaster *> instances;
-    return instances;
-  }
-
-  friend void ::HAL_I2C_MasterTxCpltCallback(I2C_HandleTypeDef *hi2c);
-  friend void ::HAL_I2C_MasterRxCpltCallback(I2C_HandleTypeDef *hi2c);
-  friend void ::HAL_I2C_ErrorCallback(I2C_HandleTypeDef *hi2c);
 };
 
 } // namespace peripheral
