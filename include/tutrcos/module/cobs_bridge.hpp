d37d2dae565b0dc9da2dbb0cac95f1ccd4237508
Rin Iwai <rin@eyrin.jp>
2024-11-16 02:51:45 +0900

    wip

--- include/tutrcos/module/cobs_bridge.hpp
+++ include/tutrcos/module/cobs_bridge.hpp
@@ -18,39 +18,46 @@ class COBSBridge {
 public:
   COBSBridge(peripheral::UART &uart) : uart_{uart} {}
 
-  bool transmit(const uint8_t *data, size_t size, uint32_t timeout) {
-    if (tx_buf_.size() < COBS_ENCODE_DST_BUF_LEN_MAX(size) + 1) {
-      tx_buf_.resize(COBS_ENCODE_DST_BUF_LEN_MAX(size) + 1);
+  template <class T>
+  bool transmit(uint8_t id, const T &data, uint32_t timeout) {
+    // id(1 byte) + data(n byte) + checksum(1 byte) + delimiter(1 byte)
+    size_t max_encoded_size = COBS_ENCODE_DST_BUF_LEN_MAX(sizeof(T));
+    if (tx_buf_.size() < max_encoded_size + 3) {
+      tx_buf_.resize(max_encoded_size + 3);
     }
     cobs_encode_result res =
-        cobs_encode(tx_buf_.data(), tx_buf_.size(), data, size);
+        cobs_encode(tx_buf_.data() + 1, tx_buf_.size() - 3, &data, sizeof(T));
     if (res.status != COBS_ENCODE_OK) {
       return false;
     }
-    tx_buf_[res.out_len] = 0;
-    return uart_.transmit(tx_buf_.data(), res.out_len + 1, timeout);
+    tx_buf_[0] = id;
+    tx_buf_[res.out_len + 1] = 0xFF; // checksum
+    tx_buf_[res.out_len + 2] = 0;    // delimiter
+    return uart_.transmit(tx_buf_.data(), res.out_len + 3, timeout);
   }
 
-  std::optional<size_t> receive(uint8_t *data, size_t size, uint32_t timeout) {
+  template <class T> bool receive(uint8_t &id, T &data, uint32_t timeout) {
     uint32_t start = core::Kernel::get_ticks();
     while (!read_to_end()) {
       uint32_t elapsed = core::Kernel::get_ticks() - start;
       if (elapsed >= timeout) {
-        return std::nullopt;
+        return false;
       }
       core::Thread::delay(1);
     }
 
-    if (size < COBS_ENCODE_DST_BUF_LEN_MAX(rx_buf_.size())) {
+    size_t max_decoded_size = COBS_ENCODE_DST_BUF_LEN_MAX(rx_buf_.size() - 3);
+    /* if (sizeof(T) < COBS_ENCODE_DST_BUF_LEN_MAX(rx_buf_.size())) {
       return std::nullopt;
-    }
+    } */
     cobs_decode_result res =
-        cobs_decode(data, size, rx_buf_.data(), rx_buf_.size());
+        cobs_decode(&data, sizeof(T), rx_buf_.data() + 1, rx_buf_.size() - 3);
+    id = rx_buf_[0];
     rx_buf_.clear();
     if (res.status != COBS_DECODE_OK) {
-      return std::nullopt;
+      return false;
     }
-    return res.out_len;
+    return true;
   }
 
 private:
