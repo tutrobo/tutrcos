18461800e173f4add857443afd2abea80afe42b4
Rin Iwai <rin@eyrin.jp>
2024-11-16 14:56:40 +0900

    fix

--- include/tutrcos/peripheral/i2c_master.hpp
+++ include/tutrcos/peripheral/i2c_master.hpp
@@ -19,9 +19,11 @@ public:
   bool transmit(uint16_t address, uint8_t *data, size_t size,
                 uint32_t timeout) {
     std::lock_guard lock{mtx_};
+    if (HAL_I2C_Master_Transmit_IT(hi2c_, address << 1, data, size) != HAL_OK) {
+      return false;
+    }
     uint32_t start = core::Kernel::get_ticks();
-    while (HAL_I2C_Master_Transmit_IT(hi2c_, address << 1, data, size) !=
-           HAL_OK) {
+    while (hi2c_->State != HAL_I2C_STATE_READY) {
       uint32_t elapsed = core::Kernel::get_ticks() - start;
       if (elapsed >= timeout) {
         return false;
@@ -34,8 +36,10 @@ public:
   bool receive(uint16_t address, uint8_t *data, size_t size, uint32_t timeout) {
     std::lock_guard lock{mtx_};
     uint32_t start = core::Kernel::get_ticks();
-    while (HAL_I2C_Master_Receive_IT(hi2c_, address << 1, data, size) !=
-           HAL_OK) {
+    if (HAL_I2C_Master_Receive_IT(hi2c_, address << 1, data, size) != HAL_OK) {
+      return false;
+    }
+    while (hi2c_->State != HAL_I2C_STATE_READY) {
       uint32_t elapsed = core::Kernel::get_ticks() - start;
       if (elapsed >= timeout) {
         return false;
