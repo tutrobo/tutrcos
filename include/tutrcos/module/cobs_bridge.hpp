5eb502a4711166abbe7bf7a593533fc7114fd697
Rin Iwai <rin@eyrin.jp>
2024-11-19 00:00:54 +0900

    fix

--- include/tutrcos/module/cobs_bridge.hpp
+++ include/tutrcos/module/cobs_bridge.hpp
@@ -23,16 +23,21 @@ public:
 
   bool transmit(const COBSBridgeMessage &msg, uint32_t timeout) {
     // id(1 byte) + data(n byte) + checksum(1 byte) + delimiter(1 byte)
-    tx_buf_.resize(COBS_ENCODE_DST_BUF_LEN_MAX(msg.data.size()) + 3);
-    cobs_encode_result res = cobs_encode(tx_buf_.data() + 1, tx_buf_.size() - 3,
-                                         msg.data.data(), msg.data.size());
-    if (res.status != COBS_ENCODE_OK) {
-      return false;
+    if (!msg.data.empty()) {
+      tx_buf_.resize(COBS_ENCODE_DST_BUF_LEN_MAX(msg.data.size()) + 3);
+      cobs_encode_result res =
+          cobs_encode(tx_buf_.data() + 1, tx_buf_.size() - 3, msg.data.data(),
+                      msg.data.size());
+      if (res.status != COBS_ENCODE_OK) {
+        return false;
+      }
+      tx_buf_.resize(res.out_len + 3);
     }
+
     tx_buf_[0] = msg.id;
-    tx_buf_[res.out_len + 1] = checksum(tx_buf_.data(), res.out_len + 1);
-    tx_buf_[res.out_len + 2] = 0; // delimiter
-    return uart_.transmit(tx_buf_.data(), res.out_len + 3, timeout);
+    tx_buf_[tx_buf_.size() - 2] = checksum(tx_buf_.data(), tx_buf_.size() - 2);
+    tx_buf_[tx_buf_.size() - 1] = 0; // delimiter
+    return uart_.transmit(tx_buf_.data(), tx_buf_.size(), timeout);
   }
 
   bool receive(COBSBridgeMessage &msg, uint32_t timeout) {
@@ -46,6 +51,7 @@ public:
     }
 
     if (rx_buf_.size() < 3) {
+      rx_buf_.clear();
       return false;
     }
     if (checksum(rx_buf_.data(), rx_buf_.size() - 2) !=
@@ -53,17 +59,21 @@ public:
       rx_buf_.clear();
       return false;
     }
-    msg.data.resize(COBS_DECODE_DST_BUF_LEN_MAX(rx_buf_.size() - 3));
-    cobs_decode_result res =
-        cobs_decode(msg.data.data(), msg.data.size(), rx_buf_.data() + 1,
-                    rx_buf_.size() - 3);
-    if (res.status != COBS_DECODE_OK) {
-      rx_buf_.clear();
-      return false;
+
+    if (rx_buf_.size() > 3) {
+      msg.data.resize(COBS_DECODE_DST_BUF_LEN_MAX(rx_buf_.size() - 3));
+      cobs_decode_result res =
+          cobs_decode(msg.data.data(), msg.data.size(), rx_buf_.data() + 1,
+                      rx_buf_.size() - 3);
+      if (res.status != COBS_DECODE_OK) {
+        rx_buf_.clear();
+        return false;
+      }
+      msg.data.resize(res.out_len);
     }
+
     msg.id = rx_buf_[0];
     rx_buf_.clear();
-    msg.data.resize(res.out_len);
     return true;
   }
 
