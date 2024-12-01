16bfd9fb2ece8d6b1fdf7b290b4dfc4de4491b01
Rin Iwai <rin@eyrin.jp>
2024-12-01 19:18:04 +0900

    fix uart

--- src/peripheral/uart.cpp
+++ src/peripheral/uart.cpp
@@ -19,19 +19,10 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
 }
 
 void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart) {
-  HAL_UART_AbortTransmit_IT(huart);
+  TUTRCOS_VERIFY(HAL_UART_Abort_IT(huart) == HAL_OK);
 }
 
-void HAL_UART_AbortTransmitCpltCallback(UART_HandleTypeDef *huart) {
-  if (__HAL_UART_GET_FLAG(huart, UART_FLAG_PE) != RESET) {
-    __HAL_UART_CLEAR_PEFLAG(huart);
-  } else if (__HAL_UART_GET_FLAG(huart, UART_FLAG_FE) != RESET) {
-    __HAL_UART_CLEAR_FEFLAG(huart);
-  } else if (__HAL_UART_GET_FLAG(huart, UART_FLAG_NE) != RESET) {
-    __HAL_UART_CLEAR_NEFLAG(huart);
-  } else if (__HAL_UART_GET_FLAG(huart, UART_FLAG_ORE) != RESET) {
-    __HAL_UART_CLEAR_OREFLAG(huart);
-  }
+void HAL_UART_AbortCpltCallback(UART_HandleTypeDef *huart) {
   if (auto uart = tutrcos::peripheral::UART::get_instances().get(huart)) {
     uart->sem_.release();
     TUTRCOS_VERIFY(HAL_UART_Receive_IT(huart, &uart->rx_buf_, 1) == HAL_OK);
