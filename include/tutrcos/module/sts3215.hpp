865604739b2924fb4070db9c8d0d8982c2527748
DESKTOP-0KFBH1M\taku4 <taku4kusu@gmail.com>
2024-12-06 15:53:30 +0900

    fix sts3215

--- include/tutrcos/module/sts3215.hpp
+++ include/tutrcos/module/sts3215.hpp
@@ -17,6 +17,8 @@ namespace module {
 /**
  *
  * modeはSTS3215のworkmodeの設定に応じて選択してください。
+ * シリアル通信のbaudrateが1Mbpsだと、オーバーランエラーになる可能性が高いです（処理能力依存）
+ * 500kbps以下を推奨します。
  *
  * @code{.cpp}
  * #include <cstdio>
@@ -71,34 +73,28 @@ public:
 
   bool update() {
     uint8_t rx_data[8] = {0};
-    for (uint8_t i = 0;; ++i) {
-      if (i > 5) {
-        return false;
-      }
-      uart_.flush();
-      if (send({0x02, 0x38, 0x02})) {
-        // rx_data : 0xff 0xff id size cmd data data checksum
-        if (uart_.receive(rx_data, 8, 1)) {
-          uint8_t checksum = 0;
-          for (uint8_t i = 2; i < 8; i++){
-            checksum += rx_data[i];
+    uart_.flush();
+    if (send({0x02, 0x38, 0x02})) {
+      // rx_data : 0xff 0xff id size cmd data data checksum
+      if (uart_.receive(rx_data, 8, 1)) {
+        uint8_t checksum = 0;
+        for (uint8_t i = 2; i < 8; i++) {
+          checksum += rx_data[i];
+        }
+        if ((rx_data[0] == 0xff) && (rx_data[1] == 0xff) &&
+            (checksum == 0xff) && (rx_data[2] == id_)) {
+
+          int16_t count = static_cast<int16_t>(rx_data[6] << 8) | rx_data[5];
+          int16_t delta = count - prev_count_;
+
+          if (delta > (ppr_ / 2)) {
+            delta -= ppr_;
+          } else if (delta < -(ppr_ / 2)) {
+            delta += ppr_;
           }
-          if ((rx_data[0] == 0xff) && (rx_data[1] == 0xff) &&
-              (checksum == 0xff) && (rx_data[2] == id_)) {
 
-            int16_t count = static_cast<int16_t>(rx_data[6] << 8) | rx_data[5];
-            int16_t delta = count - prev_count_;
-
-            if (delta > (ppr_ / 2)) {
-              delta -= ppr_;
-            } else if (delta < -(ppr_ / 2)) {
-              delta += ppr_;
-            }
-
-            set_count(get_count() + delta);
-            prev_count_ = count;
-            break;
-          }
+          set_count(get_count() + delta);
+          prev_count_ = count;
         }
       }
     }
@@ -143,10 +139,11 @@ private:
     uint8_t size = tx.size() + 1;
     tx.insert(tx.begin(), {0xff, 0xff, id_, size});
     uint8_t checksum = 0;
-    for (uint8_t i = 2, size = tx.size(); i < size; i++)
+    for (uint8_t i = 2, size = tx.size(); i < size; i++) {
       checksum += tx[i];
+    }
     tx.emplace_back(~checksum);
-    return uart_.transmit(tx.data(), tx.size(), 1);
+    return uart_.transmit(tx.data(), size + 5, 1);
   }
 };
 
