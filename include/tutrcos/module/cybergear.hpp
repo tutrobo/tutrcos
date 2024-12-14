a5a636bae753162185ad4909d6a50f61e4cb9652
DESKTOP-0KFBH1M\taku4 <taku4kusu@gmail.com>
2024-12-14 19:58:50 +0900

    大幅ダイエットして，とりあえずほかのモーターと同じ様には使えるようにした

--- include/tutrcos/module/cybergear.hpp
+++ include/tutrcos/module/cybergear.hpp
@@ -4,6 +4,7 @@
 #include <cstddef>
 #include <cstdint>
 #include <cstring>
+#include <typeinfo>
 
 #include "tutrcos/peripheral/can_base.hpp"
 #include "tutrcos/utility.hpp"
@@ -112,57 +113,16 @@ public:
     RADPS = 2,
     CURRENT = 3,
   };
-  typedef struct {
-    unsigned long stamp_usec; //< timestamp
-    uint8_t motor_id;         //!< motor id
-    float position;           //!< encoder position (-4pi to 4pi)
-    float velocity;           //!< motor velocity (-30rad/s to 30rad/s)
-    float effort;             //!< motor effort (-12Nm - 12Nm)
-    float temperature;        //!< temperature
-    uint16_t raw_position;    //!< raw position (for sync data)
-    uint16_t raw_velocity;    //!< raw velocity (for sync data)
-    uint16_t raw_effort;      //!< raw effort (for sync data)
-    uint16_t raw_temperature; //!< raw temperature (for sync data)
-  } MotorStatus;
 
-  typedef struct {
-    bool encoder_not_calibrated;
-    bool over_current_phase_a;
-    bool over_current_phase_b;
-    bool over_voltage;
-    bool under_voltage;
-    bool driver_chip;
-    bool motor_over_tempareture;
-  } MotorFault;
+  enum class TurnMode {
+    SINGLE_TURN,
+    MULTI_TURN,
+  };
 
-  typedef struct {
-    unsigned long stamp_usec;
-    uint16_t run_mode;
-    float iq_ref;
-    float spd_ref;
-    float limit_torque;
-    float cur_kp;
-    float cur_ki;
-    float cur_filt_gain;
-    float loc_ref;
-    float limit_spd;
-    float limit_cur;
-    float mech_pos;
-    float iqf;
-    float mech_vel;
-    float vbus;
-    int16_t rotation;
-    float loc_kp;
-    float spd_kp;
-    float spd_ki;
-  } MotorParameter;
-
-  Cybergear(peripheral::CAN &can, uint8_t motor_id, uint8_t master_id)
-      : EncoderBase(1),
-        can_{
-            can,
-        },
-        motor_id_{motor_id}, master_id_{master_id} {
+  Cybergear(peripheral::CAN &can, uint8_t motor_id, uint8_t master_id,
+            TurnMode turnmode = TurnMode::MULTI_TURN)
+      : EncoderBase(1 << 14), can_{can}, motor_id_{motor_id},
+        master_id_{master_id}, turnmode_{turnmode} {
     reset();
     set_motor_mode(MotorMode::CURRENT);
     enable();
@@ -171,59 +131,40 @@ public:
   virtual ~Cybergear() {}
 
   bool update() override {
-    // readRam(ADDR_MECH_POS);
+    float rad;
+    read_paramater(ADDR_MECH_POS, rad);
+    int16_t count = (rad / (2 * M_PI)) * get_cpr();
+    int16_t delta = count - prev_count_;
+
+    switch (turnmode_) {
+    case TurnMode::SINGLE_TURN: {
+      set_count(count);
+      break;
+    }
+    case TurnMode::MULTI_TURN: {
+      if (delta > (get_cpr() / 2)) {
+        delta -= get_cpr();
+      } else if (delta < -(get_cpr() / 2)) {
+        delta += get_cpr();
+      }
+      set_count(get_count() + delta);
+      break;
+    }
+    }
+    prev_count_ = count;
     return true;
   }
 
   bool transmit() {
     enable();
-    setCurRef(current_);
+    transmit_current_ref(current_);
     return true;
   }
 
   void set_current(float ampare) { current_ = ampare; }
+  float get_current() { return current_; }
 
-  void stop() {
-    // setMode(MotorMode::NONE);
-    reset();
-    // setCurrent(0);
-  }
-
-  // void get_mech_position() { readRam(ADDR_MECH_POS); }
-
-  void readRam(uint16_t index) {
-    peripheral::CANBase::Message msg;
-    std::array<uint8_t, 8U> data; // = {0x00};
-    data.fill(0);
-    data[0] = static_cast<uint8_t>(index);
-    data[1] = static_cast<uint8_t>(index >> 8);
-    send(motor_id_, CMD_RAM_READ, master_id_, data);
-    while (can_.receive(msg, 1)) {
-      process_receive_data(msg);
-    }
-  }
-
-  void process_receive_data(const peripheral::CANBase::Message &msg) {
-    if (static_cast<uint16_t>(msg.id & 0xffff) ==
-        static_cast<uint16_t>((motor_id_ << 8) | master_id_)) {
-      uint8_t packet_type = (msg.id & 0x3F000000) >> 24;
-      switch (packet_type) {
-      case CMD_RESPONSE:
-        process_motor_packet(msg.data);
-        printf("response\r\n");
-        break;
-      case CMD_RAM_READ:
-        process_read_parameter_packet(msg.data);
-        break;
-      case CMD_GET_MOTOR_FAIL:
-        printf("error\r\n");
-        break;
-      default:
-        printf("invalid response 0x%x\r\n", packet_type);
-        break;
-      }
-    }
-  }
+  void stop() { reset(); }
 
   void enable() {
     stop_flag = false;
@@ -238,7 +179,6 @@ public:
   void set_motor_mode(MotorMode mode) {
     if (mode == MotorMode::NONE)
       return;
-    // uint8_t data[8] = {0x00};
     std::array<uint8_t, 8U> data = {0x00};
     data[0] = ADDR_RUN_MODE & 0x00FF;
     data[1] = ADDR_RUN_MODE >> 8;
@@ -246,126 +186,105 @@ public:
     send(motor_id_, CMD_RAM_WRITE, master_id_, data);
   }
 
-private:
-  void setPosRef(float value) {
-    writeFloat(motor_id_, ADDR_LOC_REF, value, POS_MIN, POS_MAX);
-  }
-  void setVelRef(float value) {
-    writeFloat(motor_id_, ADDR_SPEED_REF, value, VEL_MIN, VEL_MAX);
-  }
-  void setCurRef(float value) {
-    writeFloat(motor_id_, ADDR_IQ_REF, value, CUR_MIN, CUR_MAX);
+  template <class T> bool read_paramater(uint16_t index, T &dest) {
+    std::array<uint8_t, 8U> data; // = {0x00};
+    data.fill(0);
+    data[0] = static_cast<uint8_t>(index);
+    data[1] = static_cast<uint8_t>(index >> 8);
+    if (!send(motor_id_, CMD_RAM_READ, master_id_, data)) {
+      return false;
+    }
+    return receive(dest, CMD_RAM_READ, index, 1);
   }
 
-  bool transmit(peripheral::CANBase::IDType idtype, uint32_t id,
-                const std::array<uint8_t, 8U> &data, uint32_t timeout) {
+private:
+  peripheral::CAN &can_;
+  const uint8_t motor_id_, master_id_;
+  TurnMode turnmode_;
+
+  unsigned long send_count_;
+  bool stop_flag = true;
+  float current_;
+  int16_t prev_count_ = 0;
+
+  bool transmit_current_ref(float value) {
+    return write_float(motor_id_, ADDR_IQ_REF, value, CUR_MIN, CUR_MAX);
+  }
+
+  bool send(uint8_t can_id, uint8_t cmd_id, uint16_t option,
+            const std::array<uint8_t, 8U> &data) {
+    uint32_t id = cmd_id << 24 | option << 8 | can_id;
+    ++send_count_;
     peripheral::CANBase::Message msg;
-    msg.id_type = idtype;
+    msg.id_type = peripheral::CANBase::IDType::EXTENDED;
     msg.id = id;
     msg.data = data;
     msg.dlc = 8;
-    return can_.transmit(msg, timeout);
+    return can_.transmit(msg, 1);
   }
 
-  void process_motor_packet(const std::array<uint8_t, 8U> &data) {
-    motor_status_.raw_position = data[1] | data[0] << 8;
-    motor_status_.raw_velocity = data[3] | data[2] << 8;
-    motor_status_.raw_effort = data[5] | data[4] << 8;
-    motor_status_.raw_temperature = data[7] | data[6] << 8;
-
-    // convert motor data
-    // motor_status_.stamp_usec = micros();
-    motor_status_.motor_id = motor_id_;
-    motor_status_.position =
-        uint2float(motor_status_.raw_position, P_MIN, P_MAX);
-    motor_status_.velocity =
-        uint2float(motor_status_.raw_velocity, V_MIN, V_MAX);
-    motor_status_.effort = uint2float(motor_status_.raw_effort, T_MIN, T_MAX);
-    motor_status_.temperature = motor_status_.raw_temperature;
-  }
-
-  void process_read_parameter_packet(const std::array<uint8_t, 8U> &data) {
-    uint16_t index = data[1] << 8 | data[0];
-    uint8_t uint8_data;
-    memcpy(&uint8_data, &data[4], sizeof(uint8_t));
-    int16_t int16_data;
-    memcpy(&int16_data, &data[4], sizeof(int16_t));
-    float float_data;
-    memcpy(&float_data, &data[4], sizeof(float));
-
-    switch (index) {
-    case ADDR_RUN_MODE:
-      motor_param_.run_mode = uint8_data;
-      break;
-    case ADDR_IQ_REF:
-      motor_param_.iq_ref = float_data;
-      break;
-    case ADDR_SPEED_REF:
-      motor_param_.spd_ref = float_data;
-      break;
-    case ADDR_LIMIT_TORQUE:
-      motor_param_.limit_torque = float_data;
-      break;
-    case ADDR_CURRENT_KP:
-      motor_param_.cur_kp = float_data;
-      break;
-    case ADDR_CURRENT_KI:
-      motor_param_.cur_ki = float_data;
-      break;
-    case ADDR_CURRENT_FILTER_GAIN:
-      motor_param_.cur_filt_gain = float_data;
-      break;
-    case ADDR_LOC_REF:
-      motor_param_.loc_ref = float_data;
-      break;
-    case ADDR_LIMIT_SPEED:
-      motor_param_.limit_spd = float_data;
-      break;
-    case ADDR_LIMIT_CURRENT:
-      motor_param_.limit_cur = float_data;
-      break;
-    case ADDR_MECH_POS:
-      motor_param_.mech_pos = float_data;
-      break;
-    case ADDR_IQF:
-      motor_param_.iqf = float_data;
-      break;
-    case ADDR_MECH_VEL:
-      motor_param_.mech_vel = float_data;
-      break;
-    case ADDR_VBUS:
-      motor_param_.vbus = float_data;
-      break;
-    case ADDR_ROTATION:
-      motor_param_.rotation = int16_data;
-      break;
-    case ADDR_LOC_KP:
-      motor_param_.loc_kp = float_data;
-      break;
-    case ADDR_SPD_KP:
-      motor_param_.spd_kp = float_data;
-      break;
-    case ADDR_SPD_KI:
-      motor_param_.spd_ki = float_data;
-      break;
-    default:
-      // is_updated = false;
-      break;
+  template <class T>
+  bool receive(T &dest, uint8_t cmd, uint16_t index, uint32_t timeout) {
+    peripheral::CANBase::Message msg;
+    uint32_t start = core::Kernel::get_ticks();
+    uint32_t elapsed = start;
+    while (true) {
+      bool res = can_.receive(msg, 0);
+      if (res) {
+        uint16_t id[] = {
+            static_cast<uint16_t>(msg.id & 0xffff),
+            static_cast<uint16_t>((motor_id_ << 8) | master_id_),
+        };
+        uint8_t rx_cmd = (msg.id & 0x3F000000) >> 24;
+        if ((cmd == CMD_RAM_READ) && (rx_cmd == cmd) && (id[0] == id[1])) {
+          // switch (cmd) {
+          // case CMD_RESPONSE: // 応答
+          //   process_motor_packet(msg.data);
+          //   return true;
+          //   break;
+          // case CMD_RAM_READ: //
+          if (process_read_parameter_packet(dest, index, msg.data)) {
+            return true;
+          }
+          //   break;
+          // default:
+          //   break;
+          // }
+        }
+      }
+      elapsed = core::Kernel::get_ticks();
+      if ((elapsed - start) >= timeout) {
+        return false;
+      }
+      core::Thread::delay(1);
     }
-    // if (is_updated) {
-    //   motor_param_.stamp_usec = micros();
-    // }
+    return false;
   }
 
-  void send(uint8_t can_id, uint8_t cmd_id, uint16_t option,
-            const std::array<uint8_t, 8U> &data) {
-    uint32_t id = cmd_id << 24 | option << 8 | can_id;
-    transmit(peripheral::CANBase::IDType::EXTENDED, id, data, 1);
-    ++send_count_;
+  template <class T>
+  bool process_read_parameter_packet(T &dest, uint16_t index,
+                                     const std::array<uint8_t, 8U> &data) {
+    uint16_t rx_index = data[1] << 8 | data[0];
+    if (rx_index == index) {
+      switch (index) {
+      case ADDR_RUN_MODE:
+        memcpy(&dest, &data[4], sizeof(uint8_t));
+        break;
+      case ADDR_ROTATION:
+        memcpy(&dest, &data[4], sizeof(int16_t));
+        break;
+      default:
+        memcpy(&dest, &data[4], sizeof(float));
+        break;
+      }
+      return true;
+    } else {
+      return false;
+    }
   }
 
-  void writeFloat(uint8_t can_id, uint16_t addr, float value, float min,
-                  float max) {
+  bool write_float(uint8_t can_id, uint16_t addr, float value, float min,
+                   float max) {
     std::array<uint8_t, 8U> data = {0x00};
     data[0] = addr & 0x00FF;
     data[1] = addr >> 8;
@@ -380,7 +299,7 @@ private:
     data[6] = val.data[2];
     data[7] = val.data[3];
 
-    send(can_id, CMD_RAM_WRITE, master_id_, data);
+    return send(can_id, CMD_RAM_WRITE, master_id_, data);
   }
 
   int float2unit(float x, float x_min, float x_max, int bits) {
@@ -395,173 +314,41 @@ private:
     float span = x_max - x_min;
     return (float)x / type_max * span + x_min;
   }
-
-  // void setMechPos2Zero() {
-  //   std::array<uint8_t, 8U> data = {0x00};
-  //   data[0] = 0x01;
-  //   send(motor_id_, CMD_SET_MECH_POSITION_TO_ZERO, master_id_, data);
-  // }
-
-  // void changeId(uint8_t can_id) {
-  //   std::array<uint8_t, 8U> data = {0x00};
-  //   uint16_t option = can_id << 8 | master_id_;
-  //   send(motor_id_, CMD_CHANGE_CAN_ID, option, data);
-  // }
-
-  void processPacket(const std::array<uint8_t, 8U> &data) {
-    motor_status_.raw_position = data[1] | data[0] << 8;
-    motor_status_.raw_velocity = data[3] | data[2] << 8;
-    motor_status_.raw_effort = data[5] | data[4] << 8;
-    motor_status_.raw_temperature = data[7] | data[6] << 8;
-
-    // convert motor data
-    // motor_status_.stamp_usec = micros();
-    motor_status_.motor_id = motor_id_;
-    motor_status_.position =
-        uint2float(motor_status_.raw_position, POS_MIN, POS_MAX);
-    motor_status_.velocity =
-        uint2float(motor_status_.raw_velocity, VEL_MIN, VEL_MAX);
-    motor_status_.effort =
-        uint2float(motor_status_.raw_effort, TOR_MIN, TOR_MAX);
-    motor_status_.temperature = motor_status_.raw_temperature;
-  }
-
-  peripheral::CAN &can_;
-  MotorStatus motor_status_;
-  MotorParameter motor_param_;
-
-  const uint8_t motor_id_, master_id_;
-  uint8_t receive_buffer_[64];
-  unsigned long send_count_;
-  bool stop_flag = true;
-  float current_;
 };
-
-// void set_limit_speed(float speed) { writeFloat(motor_id_, ADDR_LIMIT_SPEED,
-// speed, 0.0f, VEL_MAX); } void set_limit_current(float current) {
-// writeFloat(motor_id_, ADDR_LIMIT_CURRENT, current, 0.0f, CUR_MAX); } void
-// set_current_kp(float kp) { writeFloat(motor_id_, ADDR_CURRENT_KP, kp, 0.0f,
-// CUR_KP_MAX); } void set_current_ki(float ki) { writeFloat(motor_id_,
-// ADDR_CURRENT_KI, ki, 0.0f, CUR_KI_MAX); } void set_current_filter_gain(float
-// gain) { writeFloat(motor_id_, ADDR_CURRENT_FILTER_GAIN, gain,
-// CURRENT_FILTER_GAIN_MIN, CURRENT_FILTER_GAIN_MAX); } void
-// set_limit_torque(float torque) { writeFloat(motor_id_, ADDR_LIMIT_TORQUE,
-// torque, 0.0f, TOR_MAX); } void set_position_kp(float kp) {
-// writeFloat(motor_id_, ADDR_LOC_KP, kp, 0.0f, 200.0f); } void
-// set_velocity_kp(float kp) { writeFloat(motor_id_, ADDR_SPD_KP, kp, 0.0f,
-// 200.0f); } void set_velocity_ki(float ki) { writeFloat(motor_id_,
-// ADDR_SPD_KI, ki, 0.0f, 200.0f); } void get_mech_position() {
-// readRam(ADDR_MECH_POS); } void get_mech_velocity() { readRam(ADDR_MECH_VEL);
-// } void get_vbus() { readRam(ADDR_VBUS); } void get_rotation() {
-// readRam(ADDR_ROTATION); }
-
-// void motor_control(float position, float speed, float torque, float kp, float
-// kd) {
-//   std::array<uint8_t, 8U> data
-//   data[0] = float2unit(position, POS_MIN, POS_MAX, 16) >> 8;
-//   data[1] = float2unit(position, POS_MIN, POS_MAX, 16);
-//   data[2] = float2unit(speed, VEL_MIN, VEL_MAX, 16) >> 8;
-//   data[3] = float2unit(speed, VEL_MIN, VEL_MAX, 16);
-//   data[4] = float2unit(kp, KP_MIN, KP_MAX, 16) >> 8;
-//   data[5] = float2unit(kp, KP_MIN, KP_MAX, 16);
-//   data[6] = float2unit(kd, KD_MIN, KD_MAX, 16) >> 8;
-//   data[7] = float2unit(kd, KD_MIN, KD_MAX, 16);
-
-//   uint16_t data_torque = float2unit(torque, TOR_MIN, TOR_MAX, 16);
-//   send(motor_id_, CMD_POSITION, data_torque, data);
-// }
-
-// void dump_motor_param() {
-//     const std::vector<uint16_t> index_array = {
-//         ADDR_RUN_MODE,
-//         ADDR_IQ_REF,
-//         ADDR_SPEED_REF,
-//         ADDR_LIMIT_TORQUE,
-//         ADDR_CURRENT_KP,
-//         ADDR_CURRENT_KI,
-//         ADDR_CURRENT_FILTER_GAIN,
-//         ADDR_LOC_REF,
-//         ADDR_LIMIT_SPEED,
-//         ADDR_LIMIT_CURRENT,
-//         ADDR_MECH_POS,
-//         ADDR_IQF,
-//         ADDR_MECH_VEL,
-//         ADDR_VBUS,
-//         ADDR_ROTATION,
-//         ADDR_LOC_KP,
-//         ADDR_SPD_KP,
-//         ADDR_SPD_KI
-//     };
-
-//     for (auto index : index_array){
-//         read_ram_data(index);
-//         // delay(1);
-//     }
-// }
-// uint8_t getMode() const { return run_mode_; }
-
-// uint8_t getId() const { return motor_id_; }
-
-// bool process_packet() {
-//   bool check_update = false;
-//   while (true) {
-//     if (receive_motor_data(motor_status_)) {
-//       check_update = true;
-//     } else {
-//       break;
-//     }
-//   }
-//   return check_update;
-// }
-
-// bool update_motor_status(unsigned long id, const uint8_t * data, unsigned
-// long len){
-//   uint8_t receive_can_id = id & 0xff;
-//   if ( receive_can_id != master_id_ ) {
-//     return false;
-//   }
-//   uint8_t motor_can_id = (id & 0xff00) >> 8;
-//   if ( motor_can_id != motor_id_ ) {
-//     return false;
-//   }
-//   // check packet type
-//   uint8_t packet_type = (id & 0x3F000000) >> 24;
-//   if (packet_type == CMD_RESPONSE) {
-//     process_motor_packet(data, len);
-//   } else if (packet_type == CMD_RAM_READ){
-//     process_read_parameter_packet(data, len);
-//   } else if (packet_type == CMD_GET_MOTOR_FAIL) {
-//     // NOT IMPLEMENTED
-//   } else {
-//     printf("invalid command response [0x%x]\n", packet_type);
-//     print_can_packet(id, data, len);
-//     return false;
-//   }
-//   return true;
-// }
-
-// bool receive_motor_data(MotorStatus & mot) {
-//   // receive data
-//   unsigned long id;
-//   uint8_t len;
-//   if (!can_->read_message(id, receive_buffer_, len)) {
-//     printf("received data is not available\r\n");
-//     return false;
-//   }
-//   // if id is not mine
-//   uint8_t receive_can_id = id & 0xff;
-//   if ( receive_can_id != master_id_ ) {
-//     printf("Invalid master can id. Expected=[0x%02x] Actual=[0x%02x]
-//     Raw=[%x]\r\n", master_id_, receive_can_id, id); return false;
-//   }
-//   uint8_t motor_can_id = (id & 0xff00) >> 8;
-//   if ( motor_can_id != motor_id_ ) {
-//     printf("Invalid target can id. Expected=[0x%02x] Actual=[0x%02x]
-//     Raw=[%x]\r\n", motor_id_, motor_can_id, id); return false;
-//   }
-//   // parse packet --------------
-//   return update_motor_status(id, receive_buffer_, len);
-// }
 
 } // namespace module
 } // namespace tutrcos
+
+// bool setPosRef(float value) {
+//   return write_float(motor_id_, ADDR_LOC_REF, value, POS_MIN, POS_MAX);
+// }
+// bool setVelRef(float value) {
+//   return write_float(motor_id_, ADDR_SPEED_REF, value, VEL_MIN, VEL_MAX);
+// }
+
+// void setMechPos2Zero() {
+//   std::array<uint8_t, 8U> data = {0x00};
+//   data[0] = 0x01;
+//   send(motor_id_, CMD_SET_MECH_POSITION_TO_ZERO, master_id_, data);
+// }
+
+// void changeId(uint8_t can_id) {
+//   std::array<uint8_t, 8U> data = {0x00};
+//   uint16_t option = can_id << 8 | master_id_;
+//   send(motor_id_, CMD_CHANGE_CAN_ID, option, data);
+// }
+
+// void process_motor_packet(const std::array<uint8_t, 8U> &data) {
+// motor_status_.raw_position = data[1] | data[0] << 8;
+// motor_status_.raw_velocity = data[3] | data[2] << 8;
+// motor_status_.raw_effort = data[5] | data[4] << 8;
+// motor_status_.raw_temperature = data[7] | data[6] << 8;
+
+// motor_status_.motor_id = motor_id_;
+// motor_status_.position =
+//     uint2float(motor_status_.raw_position, P_MIN, P_MAX);
+// motor_status_.velocity =
+//     uint2float(motor_status_.raw_velocity, V_MIN, V_MAX);
+// motor_status_.effort = uint2float(motor_status_.raw_effort, T_MIN,
+// T_MAX); motor_status_.temperature = motor_status_.raw_temperature;
+// }
